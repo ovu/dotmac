1d21ac8016be6c71d59599ed6e38780ab6ba8c10
Omar Diego <ovu@Omars-MacBook-Pro.fritz.box>
2021-04-02 02:30:19 -0700

    Configure tmux and fonts

---
+++ roles/fish/files/config.fish
@@ -0,0 +1,19 @@
+alias v="vim"
+alias x="exit"
+alias g="git"
+
+set fish_color_command "purple"
+set fish_color_autosuggestion "yellow"
+
+set -gx PATH $HOME/Library/Python/3.9/bin $PATH
+# Set env  EDITOR used by git-mediate
+set -gx EDITOR vim
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+# history across fishes
+function save_history --on-event fish_preexec
+    history --save
+end
+alias hr 'history --merge'  # read and merge history from disk
+bind \e\[A 'history --merge ; up-or-search'
---
+++ roles/user-profile/files/.profile
@@ -0,0 +1,2 @@
+# Required for powerline-config
+# export PATH=$PATH:$HOME/Library/Python/3.9/bin
