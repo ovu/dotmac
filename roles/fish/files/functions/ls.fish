9c48b0cd11dc090f90ab1d9000901c189d53eab6
Diego Vera <omar.vera@gmail.com>
2025-01-20 23:05:41 +0100

    Better kitty config and ls

--- roles/fish/files/functions/ls.fish
+++ roles/fish/files/functions/ls.fish
@@ -1,4 +1,5 @@
 function ls --description 'always show colors with ls'
-  command gls --color=auto $argv
+  # command gls --color=auto $argv
+  command eza --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions $argv
 end
 
